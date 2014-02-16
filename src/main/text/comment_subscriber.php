0c8e256473b41d61340f9ae7f8308b3fa9eda3d8
arming <nikom2532@gmail.com>
2014-02-16 16:49:39 +0700

    fix bug comment_subscriber

--- src/main/text/comment_subscriber.php
+++ src/main/text/comment_subscriber.php
@@ -7,7 +7,7 @@
 //##################################### start DB ##############################################
 $rootpath ="../../";
 
-	header ("location: ".$rootpath."comment.php?friend=".$friend);
+header ("location: ".$rootpath."main/text/index.php?friend=".$friend);
 
 session_start();
 require_once($rootpath."lib/func_date.php");
@@ -25,29 +25,33 @@ include($rootpath."include/index_head.php");
 
 //####################################### start function ########################################
 
-	//record the update subscriber
-	if($subscriber==1)
-	{
-		$SQL_comment="insert into social_iMing_friends(username, fnd) values('$_SESSION[su]', '$friend')";
-		$result2=mysql_query($SQL_comment);
-		if(!$result2)die(mysql_error());
-	}
-	elseif($subscriber==2)
-	{
-		$SQL_comment="DELETE FROM `social_iMing_friends` WHERE CONVERT(`social_iMing_friends`.`username` USING utf8) = '$_SESSION[su]' AND CONVERT(`social_iMing_friends`.`fnd` USING utf8) = '$friend' LIMIT 1;";
-		$result2=mysql_query($SQL_comment);
-		if(!$result2)die(mysql_error());
-	}
-
-//	$url = "location: ".$rootpath."comment2.php?friend=".$friend;
-//	echo $url;
-//	header ("$url");
-//	header ("location: comment2.php?friend=$friend")
-
-
+		//record the update subscriber
+		if($subscriber==1)
+		{
+			$SQL_comment="insert into social_iMing_friends(username, fnd) values('$_SESSION[su]', '$friend')";
+			$result2=mysql_query($SQL_comment);
+			if(!$result2)die(mysql_error());
+		}
+		elseif($subscriber==2)
+		{
+			$SQL_comment="DELETE FROM `social_iMing_friends` WHERE CONVERT(`social_iMing_friends`.`username` USING utf8) = '$_SESSION[su]' AND CONVERT(`social_iMing_friends`.`fnd` USING utf8) = '$friend' LIMIT 1;";
+			$result2=mysql_query($SQL_comment);
+			if(!$result2)die(mysql_error());
+		}
+	
+	//	$url = "location: ".$rootpath."comment2.php?friend=".$friend;
+	//	echo $url;
+	//	header ("$url");
+	//	header ("location: comment2.php?friend=$friend")
+	
+	
 //###################################### end function ###########################################
-
-include($rootpath."include/index_bottom.php");
-//mysql_close();
-}	//end check login
+	
+	include($rootpath."include/index_bottom.php");
+	
+	// header ("location: ".$rootpath."comment.php?friend=".$friend);
+/*	?><script>window.location.href = "<?php echo $rootpath; ?>main/text/index.php?friend=".$friend";</script><?php
+*/	
+	//mysql_close();
+	}	//end check login
 ?>
